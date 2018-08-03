80634e0cd4f7dcf0de471ef95ca451d62e169bed
nave-en <32920857+nave-en@users.noreply.github.com>
2018-08-03 11:28:32 +0530

    Add files via upload

---
+++ scrapy.py
@@ -0,0 +1,19 @@
+import urllib.request
+
+import json
+
+print("enter the query as word")
+
+name=input()
+
+name=name.replace(" ","")
+
+link = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+name+"&limit=100&format=json"
+
+f = urllib.request.urlopen(link)
+
+myfile = f.read()
+
+x=json.loads(myfile)
+
+print(', '.join(x[2]))
